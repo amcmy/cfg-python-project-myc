b3d3ffa07c63974c2e81ae89d009d839a01b84ea
amcmy <b8037419@my.shu.ac.uk>
2019-02-27 19:40:40 +0000

    27FEB2019_1940

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+#from flask import Flask
+
+#app = Flask(__name__)
+
+#@app.route("/") #decorator
+#def say_hello():
+#  return "hello world!"
+
+#app.run(debug=True) #automatic restart itself GREAT FOR WEB DEVELOPMENT
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def say_hello():
-  return "hello world!"
+  return render_template("index.html")
 
 app.run(debug=True)
