e4d802ebac75dd7c9a56a6ab7a40805e773366b7
amcmy <b8037419@my.shu.ac.uk>
2019-03-27 07:04:56 +0000

    27MAR0704

--- app.py
+++ app.py
@@ -7,7 +7,7 @@
 #  return "hello world!"
 
 #app.run(debug=True) #automatic restart itself GREAT FOR WEB DEVELOPMENT
-
+import os #access environment variables set by Heroku, which includes the port it has assigned to run your application on.
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -32,4 +32,9 @@ def get_feedback():
 
   return render_template("feedback.html", form_data=data)
 
-app.run(debug=True)
+#The following piece of conditional logic checks whether you are running your application on Heroku (in which case, the PORT environment variable would exist), or if you are running your application locally. The host=0.0.0.0 bit instructs your Flask application to listen on all web address. This is important to ensure the application runs on Heroku since we don't know which host Heroku will decide to host the application on.
+
+if 'PORT' in os.environ:
+     app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+else:
+     app.run(debug=True)
